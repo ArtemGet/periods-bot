461325a595170bafc121d030c4ca141ce03c35e6
Artem Getmanskii <re910@yandex.ru>
2024-02-13 02:02:58 +0300

    fix .* imports

--- src/main/java/aget/periodsbot/PeriodsbotApplication.java
+++ src/main/java/aget/periodsbot/PeriodsbotApplication.java
@@ -1,10 +1,24 @@
 package aget.periodsbot;
 
-
 import aget.periodsbot.bot.PeriodsBot;
-import aget.periodsbot.bot.command.*;
-import aget.periodsbot.bot.convert.*;
-import aget.periodsbot.bot.route.*;
+import aget.periodsbot.bot.command.CurrPeriodCommand;
+import aget.periodsbot.bot.command.ErrorCommand;
+import aget.periodsbot.bot.command.LastPeriodsStatsCommand;
+import aget.periodsbot.bot.command.PeriodAddCommand;
+import aget.periodsbot.bot.command.UserGreetCommand;
+import aget.periodsbot.bot.convert.DateRqConvert;
+import aget.periodsbot.bot.convert.DateRsConvert;
+import aget.periodsbot.bot.convert.LastPeriodStatsConvert;
+import aget.periodsbot.bot.convert.PeriodAddConvert;
+import aget.periodsbot.bot.convert.PeriodsStatsConvert;
+import aget.periodsbot.bot.convert.UserGreetRqConvert;
+import aget.periodsbot.bot.convert.UserGreetRsConvert;
+import aget.periodsbot.bot.convert.UserTIdConvert;
+import aget.periodsbot.bot.route.BasicRoute;
+import aget.periodsbot.bot.route.DeadEndRoute;
+import aget.periodsbot.bot.route.InterceptErrorRoute;
+import aget.periodsbot.bot.route.TelegramCommandRoute;
+import aget.periodsbot.bot.route.TxtCommandRoute;
 import aget.periodsbot.config.BotProps;
 import aget.periodsbot.config.PgProps;
 import aget.periodsbot.domain.usecase.CurrPeriod;
